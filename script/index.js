1173664f8b897e109ebdccb96f4553332987a054
Bruno Furtado <bsouzacc@gmail.com>
2024-06-24 12:53:10 -0300

    update /links do site

--- script/index.js
+++ script/index.js
@@ -1,6 +1,6 @@
 const swiper = new Swiper('.professores__lista', {
     // Optional parameters
-    slidesPerView: 4,
+    slidesPerView: 3,
     spaceBetween: 30,
     // freeMode: true,
     direction: 'horizontal',
@@ -15,7 +15,26 @@ const swiper = new Swiper('.professores__lista', {
     navigation: {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
-    }
+    },
+
+    breakpoints: {
+        "@0.00": {
+          slidesPerView: 1,
+          spaceBetween: 20,
+        },
+        "@0.75": {
+          slidesPerView: 2,
+          spaceBetween: 20,
+        },
+        "@1.00": {
+          slidesPerView: 3,
+          spaceBetween: 40,
+        },
+        "@1.50": {
+          slidesPerView: 4,
+          spaceBetween: 50,
+        },
+      },
 });
 
 
